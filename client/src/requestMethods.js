3f1911828527af74b34201707bd1c795c0b618a2
anhy43 <87054199+angushy@users.noreply.github.com>
2022-08-14 21:01:23 +0800

    my first commit

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api/";
-// const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const BASE_URL = "https://angeshop.herokuapp.com/api/";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
-// export const publicRequest = axios.create({
-//   baseURL: BASE_URL,
-// });
+export const publicRequest = axios.create({
+  baseURL: BASE_URL,
+});
 
 export const axiosInstance = axios.create({
   baseURL: "https://angeshop.herokuapp.com/api/",
 });
 
-// export const userRequest = axios.create({
-//   baseURL: BASE_URL,
-//   header: { token: `Bearer ${TOKEN}` },
-// });
+export const userRequest = axios.create({
+  baseURL: BASE_URL,
+  header: { token: `Bearer ${TOKEN}` },
+});
